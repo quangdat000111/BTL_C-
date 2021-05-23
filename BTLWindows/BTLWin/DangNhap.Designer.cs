509b81ffb15eee300d7d6110fefafc20af1577cc
= <=>
2021-05-23 10:58:29 +0700

    Cap nhat form DangNhap, recommend

--- BTLWindows/BTLWin/DangNhap.Designer.cs
+++ BTLWindows/BTLWin/DangNhap.Designer.cs
@@ -45,6 +45,8 @@
             this.chkHienThiMK = new System.Windows.Forms.CheckBox();
             this.txtDangNhap = new System.Windows.Forms.TextBox();
             this.lblSaiTKorMK = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -110,7 +112,7 @@
             this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDangNhap.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDangNhap.ForeColor = System.Drawing.Color.Honeydew;
-            this.btnDangNhap.Location = new System.Drawing.Point(96, 615);
+            this.btnDangNhap.Location = new System.Drawing.Point(96, 616);
             this.btnDangNhap.Name = "btnDangNhap";
             this.btnDangNhap.Size = new System.Drawing.Size(388, 50);
             this.btnDangNhap.TabIndex = 4;
@@ -121,7 +123,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(96, 370);
+            this.pictureBox2.Location = new System.Drawing.Point(106, 344);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(52, 37);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -131,7 +133,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(96, 452);
+            this.pictureBox3.Location = new System.Drawing.Point(106, 426);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(52, 37);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -141,7 +143,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel2.Location = new System.Drawing.Point(154, 488);
+            this.panel2.Location = new System.Drawing.Point(164, 462);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(330, 1);
             this.panel2.TabIndex = 13;
@@ -149,7 +151,7 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel3.Location = new System.Drawing.Point(154, 406);
+            this.panel3.Location = new System.Drawing.Point(164, 380);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(330, 1);
             this.panel3.TabIndex = 14;
@@ -158,12 +160,13 @@
             // 
             this.txtMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtMatKhau.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMatKhau.Location = new System.Drawing.Point(154, 450);
+            this.txtMatKhau.Location = new System.Drawing.Point(164, 424);
             this.txtMatKhau.MaxLength = 14;
             this.txtMatKhau.Name = "txtMatKhau";
             this.txtMatKhau.Size = new System.Drawing.Size(292, 25);
             this.txtMatKhau.TabIndex = 3;
             this.txtMatKhau.Text = "Mật khẩu";
+            this.txtMatKhau.Click += new System.EventHandler(this.txtMatKhau_Click);
             this.txtMatKhau.Validating += new System.ComponentModel.CancelEventHandler(this.txtMatKhau_Validating);
             // 
             // errorProvider1
@@ -179,7 +182,7 @@
             this.chkHienThiMK.AutoSize = true;
             this.chkHienThiMK.Checked = true;
             this.chkHienThiMK.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkHienThiMK.Location = new System.Drawing.Point(96, 569);
+            this.chkHienThiMK.Location = new System.Drawing.Point(96, 570);
             this.chkHienThiMK.Name = "chkHienThiMK";
             this.chkHienThiMK.Size = new System.Drawing.Size(140, 21);
             this.chkHienThiMK.TabIndex = 0;
@@ -191,12 +194,13 @@
             // 
             this.txtDangNhap.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtDangNhap.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDangNhap.Location = new System.Drawing.Point(154, 375);
+            this.txtDangNhap.Location = new System.Drawing.Point(164, 349);
             this.txtDangNhap.MaxLength = 14;
             this.txtDangNhap.Name = "txtDangNhap";
             this.txtDangNhap.Size = new System.Drawing.Size(292, 25);
             this.txtDangNhap.TabIndex = 2;
             this.txtDangNhap.Text = "Tên đăng nhập";
+            this.txtDangNhap.Click += new System.EventHandler(this.txtDangNhap_Click);
             this.txtDangNhap.Validating += new System.ComponentModel.CancelEventHandler(this.txtDangNhap_Validating);
             // 
             // lblSaiTKorMK
@@ -204,18 +208,45 @@
             this.lblSaiTKorMK.AutoSize = true;
             this.lblSaiTKorMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSaiTKorMK.ForeColor = System.Drawing.Color.Red;
-            this.lblSaiTKorMK.Location = new System.Drawing.Point(159, 503);
+            this.lblSaiTKorMK.Location = new System.Drawing.Point(159, 484);
             this.lblSaiTKorMK.Name = "lblSaiTKorMK";
             this.lblSaiTKorMK.Size = new System.Drawing.Size(262, 25);
             this.lblSaiTKorMK.TabIndex = 1;
             this.lblSaiTKorMK.Text = "Tài khoản hoặc mật khẩu sai";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Giảng viên",
+            "Sinh viên",
+            "Quản trị viên"});
+            this.comboBox1.Location = new System.Drawing.Point(248, 531);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 28);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 534);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Loại tài khoản";
+            // 
             // DangNhap
             // 
+            this.AcceptButton = this.btnDangNhap;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(566, 718);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.lblSaiTKorMK);
             this.Controls.Add(this.txtDangNhap);
             this.Controls.Add(this.chkHienThiMK);
@@ -260,5 +291,7 @@
         private System.Windows.Forms.CheckBox chkHienThiMK;
         private System.Windows.Forms.TextBox txtDangNhap;
         private System.Windows.Forms.Label lblSaiTKorMK;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BTLWindows/BTLWin/DangNhap.cs
+++ BTLWindows/BTLWin/DangNhap.cs
@@ -14,6 +14,13 @@ namespace BTLWin
 {
     public partial class DangNhap : Form
     {
+        /*
+         * Biến mov lưu trạng thái của chuột
+         * mov = true khi mousedown (ấn chuột xuống)
+         * mov = false khi mouseup (thả chuột lên)
+         * movX, movY dùng để lưu tọa độ chuột
+         * Dùng cho việc di chuyển form đăng nhập
+         */
         bool mov;
         int movX, movY;
         public DangNhap()
@@ -21,6 +28,7 @@ namespace BTLWin
             InitializeComponent();
         }
 
+        //btnExit và btnMinimized dùng để thoát, thu xuống thanh taskbar
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -32,6 +40,12 @@ namespace BTLWin
             this.WindowState = FormWindowState.Minimized;
         }
 
+        /*
+         * Hàm panel1_MouseDown, Move, Up dùng để thực hiện việc di chuyển form đăng nhập
+         * MouseDown thực hiện lưu tọa độ của con chuột
+         * MouseMove thực hiện di chuyển form dựa theo tọa độ con chuột, thực hiện khi mousedown
+         * MouseUp kết thúc quá trình, đưa mov = false
+         */
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
             //Khi ấn chuột xuống lấy tọa độ của chuột so với form
@@ -40,15 +54,28 @@ namespace BTLWin
             mov = true;
         }
 
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov)// Nêu chuột còn đang được ấn xuống và di chuyển
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+                //MousePosition là vị trí của chuột so với màn hình 
+            }
+        }
+
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
             //Nhả chuột ra
             mov = false;
         }
 
+        /*
+         * Hàm DangNhap_Load
+         * Mặc định combobox là Giảng viên
+         */
         private void DangNhap_Load(object sender, EventArgs e)
         {
-            //Hiển thị mật khẩu
+            comboBox1.Text = "Giảng viên";
             if (chkHienThiMK.Checked)
             {
                 txtMatKhau.UseSystemPasswordChar = false;
@@ -56,6 +83,10 @@ namespace BTLWin
             lblSaiTKorMK.Visible = false;//Ẩn thông báo sai mk hoặc tên đăng nhập
         }
 
+        /*
+         * Hàm txtDangNhap_Validating thực hiện khi đang edit text
+         * Đảm bảo việc không được để trống
+         */
         private void txtDangNhap_Validating(object sender, CancelEventArgs e)
         {
             if (lblSaiTKorMK.Visible == true)
@@ -74,6 +105,7 @@ namespace BTLWin
             }
         }
 
+        //Tương tự như txtDangNhap
         private void txtMatKhau_Validating(object sender, CancelEventArgs e)
         {
             if (lblSaiTKorMK.Visible == true)
@@ -92,6 +124,9 @@ namespace BTLWin
             }
         }
 
+        /*
+         * chkHienThiMK dùng để thay đổi định dạng password cho txtMatKhau
+         */
         private void chkHienThiMK_CheckedChanged(object sender, EventArgs e)
         {
 
@@ -107,31 +142,88 @@ namespace BTLWin
             }
         }
 
+        /*
+         * btnDangNhap thực hiện đăng nhập
+         * Kiểm tra xem tài khoản hợp lệ không
+         * Nếu không hợp lệ thì báo
+         * Nếu hợp lệ thì thực hiện đăng nhập
+         */
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
 
-            DataTable table = new Database().SelectData("EXEC DangNhap_GV '" + txtDangNhap.Text + "', '" + txtMatKhau.Text + "'");
-            if(table != null)
+            try
             {
-                if (table.Rows.Count != 0)
+                /*
+                * TK giảng viên có type = 0
+                * TK sinh viên có type = 1
+                * TK TK quản trị có type 
+                */
+                if (comboBox1.Text == "Giảng viên")
                 {
-                    this.Hide();
-                    //Truyền tên đăng nhập, mật khẩu và mã giáo viên vào main form
-                    new MainForm(txtDangNhap.Text, txtMatKhau.Text, table.Rows[0][2].ToString(), 2).ShowDialog();
-                    this.Close();
+                    DataTable table = new Database().SelectData("EXEC DangNhap_GV '" + txtDangNhap.Text + "', '" + txtMatKhau.Text + "'");
+                    if (table.Rows.Count != 0)
+                    {
+                        this.Hide();
+                        //Truyền tên đăng nhập, mật khẩu và mã giáo viên vào main form
+                        new MainForm(txtDangNhap.Text, txtMatKhau.Text, table.Rows[0][2].ToString(), 0).ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        lblSaiTKorMK.Visible = true;
+                    }
                 }
-                else
+                else if(comboBox1.Text == "Sinh viên")
                 {
-                    lblSaiTKorMK.Visible = true;
+                    DataTable table = new Database().SelectData("EXEC DangNhap_SV '" + txtDangNhap.Text + "', '" + txtMatKhau.Text + "'");
+                    if (table.Rows.Count != 0)
+                    {
+                        this.Hide();
+                        //Truyền tên đăng nhập, mật khẩu và mã giáo viên vào main form
+                        new MainForm(txtDangNhap.Text, txtMatKhau.Text, table.Rows[0][2].ToString(), 1).ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        lblSaiTKorMK.Visible = true;
+                    }
+                }
+                else if(comboBox1.Text == "Quản trị viên")
+                {
+                    DataTable table = new Database().SelectData("EXEC DangNhap_QTV '" + txtDangNhap.Text + "', '" + txtMatKhau.Text + "'");
+                    if (table.Rows.Count != 0)
+                    {
+                        this.Hide();
+                        //Truyền tên đăng nhập, mật khẩu và mã giáo viên vào main form
+                        new MainForm(txtDangNhap.Text, txtMatKhau.Text, "0", 2).ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        lblSaiTKorMK.Visible = true;
+                    }
                 }
             }
-        }
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (mov)// Nêu chuột còn đang được ấn xuống và di chuyển
+            catch (Exception ex)
             {
-                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
-                //MousePosition là vị trí của chuột so với màn hình 
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Hàm Click thực hiện xóa dòng Tên đăng nhập, Mật khẩu
+        private void txtDangNhap_Click(object sender, EventArgs e)
+        {
+            if(txtDangNhap.Text == "Tên đăng nhập")
+            {
+                txtDangNhap.Clear();
+            }
+        }
+
+        private void txtMatKhau_Click(object sender, EventArgs e)
+        {
+            if(txtMatKhau.Text == "Mật khẩu")
+            {
+                txtMatKhau.Clear();
             }
         }
     }
